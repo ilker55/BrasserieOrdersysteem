c25d5d2e806c25282c29494887b2bb6f90c18954
Ilker Mengi <ilker@mengi.nl>
2023-12-16 20:36:19 +0100

    Added customers context with in-memory database

---
+++ BrasserieOrdersysteem/Repositories/CustomersContext.cs
@@ -0,0 +1,14 @@
+﻿using BrasserieOrdersysteem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrasserieOrdersysteem.Repositories
+{
+    public class CustomersContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+
+        public CustomersContext(DbContextOptions<CustomersContext> options) : base(options)
+        {
+        }
+    }
+}

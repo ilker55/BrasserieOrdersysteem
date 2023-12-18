7d41c0f0501c0c7e0c17a0472567e69436924174
Ilker Mengi <ilker@mengi.nl>
2023-12-19 00:47:45 +0100

    Added order repository

---
+++ BrasserieOrdersysteem/DAL/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using BrasserieOrdersysteem.Models;
+
+namespace BrasserieOrdersysteem.DAL
+{
+    public interface IOrderRepository
+    {
+        void AddOrder(Order order);
+        void UpdateOrder(Order order);
+        bool DeleteOrder(int orderId);
+        void Save();
+    }
+}
---
+++ BrasserieOrdersysteem/DAL/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using BrasserieOrdersysteem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrasserieOrdersysteem.DAL
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OrderSystemContext context;
+
+        public OrderRepository(OrderSystemContext context)
+            => this.context = context;
+
+        public void AddOrder(Order order)
+            => context.Orders.Add(order);
+
+        public void UpdateOrder(Order order)
+            => context.Entry(order).State = EntityState.Modified;
+
+        public bool DeleteOrder(int orderId)
+        {
+            var order = context.Orders.Find(orderId);
+            if (order == null) return false;
+            context.Orders.Remove(order);
+            return true;
+        }
+
+        public void Save() => context.SaveChanges();
+    }
+}

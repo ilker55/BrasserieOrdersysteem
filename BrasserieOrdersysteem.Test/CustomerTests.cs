63abaf1e80d0fff7a24572735004bab72b0896d5
Ilker Mengi <ilker@mengi.nl>
2023-12-18 22:14:48 +0100

    Added tests for getting customers

--- BrasserieOrdersysteem.Test/CustomerTests.cs
+++ BrasserieOrdersysteem.Test/CustomerTests.cs
@@ -1,17 +1,66 @@
-﻿using BrasserieOrdersysteem.Shared.Models;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BrasserieOrdersysteem.Controllers;
+using BrasserieOrdersysteem.DAL;
+using BrasserieOrdersysteem.Models;
 using Moq;
-using System;
-using System.Collections.Generic;
+using Xunit;
 
 namespace BrasserieOrdersysteem.Test
 {
-    [TestClass]
     public class CustomerTests
     {
-        [TestMethod]
-        public void CreateCustomer()
+        [Fact]
+        public void GetCustomer()
         {
+            // Arrange
+            var mock = new Mock<ICustomerRepository>();
+            mock.Setup(m => m.GetCustomerByID(1))
+                .Returns(new Customer
+                {
+                    Id = 1,
+                    Name = "Elena Martinez"
+                });
+
+            var controller = new CustomersController(mock.Object);
+
+            // Act
+            var customer = controller.GetCustomer(1).Value;
+
+            // Assert
+            mock.Verify(r => r.GetCustomerByID(1));
+            Assert.Equal("Elena Martinez", customer.Name);
+        }
+
+        [Fact]
+        public void GetCustomers()
+        {
+            // Arrange
+            var mock = new Mock<ICustomerRepository>();
+            mock.Setup(m => m.GetCustomers())
+                .Returns(new Customer[]
+                {
+                    new() {
+                        Id = 1,
+                        Name = "Elena Martinez"
+                    },
+                    new() {
+                        Id = 2,
+                        Name = "Jonathan Turner"
+                    },
+                    new() {
+                        Id = 3,
+                        Name = "Sophie Anderson"
+                    }
+                });
+
+            var controller = new CustomersController(mock.Object);
+
+            // Act
+            var customers = controller.GetCustomers().Value;
+
+            // Assert
+            mock.Verify(r => r.GetCustomers());
+            Assert.Equal("Elena Martinez", customers[0].Name);
+            Assert.Equal("Jonathan Turner", customers[1].Name);
         }
     }
 }

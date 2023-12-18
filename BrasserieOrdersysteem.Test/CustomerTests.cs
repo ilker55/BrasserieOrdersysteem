1ca23874565f8b652a265c94dcdc31b264d6980c
Ilker Mengi <ilker@mengi.nl>
2023-12-18 02:49:25 +0100

    Added test project

---
+++ BrasserieOrdersysteem.Test/CustomerTests.cs
@@ -0,0 +1,17 @@
+﻿using BrasserieOrdersysteem.Shared.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace BrasserieOrdersysteem.Test
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void CreateCustomer()
+        {
+        }
+    }
+}
---
+++ BrasserieOrdersysteem.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BrasserieOrdersysteem.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BrasserieOrdersysteem.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eba4fa2a-87e0-46d5-8742-9e82b8f79fcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

18f58a00d7d2b572b4bace8fbe3bc27b47dc9fe5
Ilker Mengi <ilker@mengi.nl>
2023-12-19 00:48:41 +0100

    Added orders controller

---
+++ BrasserieOrdersysteem/Controllers/OrdersController.cs
@@ -0,0 +1,58 @@
+﻿using BrasserieOrdersysteem.DAL;
+using BrasserieOrdersysteem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrasserieOrdersysteem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : Controller
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public OrdersController(IOrderRepository orderRepository)
+            => this.orderRepository = orderRepository;
+
+        // POST: api/Orders
+        [HttpPost]
+        public ActionResult CreateOrder(Order order)
+        {
+            // Add new order and save
+            orderRepository.AddOrder(order);
+            orderRepository.Save();
+
+            // Return Created
+            return StatusCode(201);
+        }
+
+        // PUT: api/Orders/4
+        [HttpPut("{id}")]
+        public ActionResult UpdateOrder(int id, Order order)
+        {
+            // Validate order ID and model
+            if (id != order.Id)
+                return BadRequest();
+
+            // Update customer and save
+            orderRepository.UpdateOrder(order);
+            orderRepository.Save();
+
+            // Return NoContent
+            return NoContent();
+        }
+
+        // DELETE: api/Orders/16
+        [HttpDelete("{id}")]
+        public ActionResult DeleteOrder(int id)
+        {
+            // Delete order by ID and save
+            var deleted = orderRepository.DeleteOrder(id);
+            orderRepository.Save();
+
+            //Return NoContent on success, otherwise NotFound
+            return deleted ? NoContent() : NotFound();
+        }
+    }
+}
